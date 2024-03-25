907b58bb4a798ed04400ebf0c318af2fca44e06f
Suryakant Singh <suryakant12m@gmail.com>
2024-03-25 06:37:14 -0700

    update(1)

--- src/test/java/org/examples/assignments/Ignore.java
+++ src/test/java/org/examples/assignments/Ignore.java
@@ -1,24 +1,34 @@
 package org.examples.assignments;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-import java.security.Key;
-import java.util.concurrent.TimeUnit;
 
 public class Ignore {
-
+@Test
     public static void main(String[] args) {
        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
-       WebDriver driver = new ChromeDriver();
-       driver.get("https://www.globalsqa.com/demo-site/");
-       driver.manage().window().maximize();
-      driver.findElement(By.xpath("(//a[@ class= \"no_border\"])[1]")).click();
-      driver.findElement(By.id("s")).sendKeys("ox");
-      driver.findElement(By.id("s")).click();
+
+   WebDriver driver = new ChromeDriver();
+   driver.manage().window().maximize();
+   driver.get("https://www.google.com/");
+   driver.findElement(By.id("APjFqb")).click();
+   driver.findElement(By.id("APjFqb")).sendKeys("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+   driver.findElement(By.id("APjFqb")).sendKeys(Keys.ENTER);
+
+   driver.findElement(By.xpath("(//a[@jsname=\"UWckNb\"])[1]")).click();
+
+
+
+   driver.findElement(By.xpath("//input[@class=\"oxd-input oxd-input--focus\"]")).click();
+   driver.findElement(By.xpath("//input[@class=\"oxd-input oxd-input--focus\"]")).sendKeys("Admin");
+
+
+
+
+
 
     }
 }
